7f111067da9b4a304ef0e053c52aac78f84d61b6
Mysterio1001 <holmes9073@gmail.com>
2024-09-29 18:47:18 +0800

    frist commit

---
+++ script.js
@@ -0,0 +1,49 @@
+//內建方法
+// console.log("hello js")
+// console.log("Hi,js")
+
+//變數宣告
+
+// let name = "Jack"
+// console.log(name)
+// let sentence = "今天天氣真好~!"
+// console.log(name + sentence)
+
+//內建方法 prompt()、alert()
+
+
+// fruit = prompt("你最喜歡的水果是什麼?")
+// console.log(fruit) //一般印出
+// alert("我喜歡" + fruit) //跳出視窗
+
+
+// let food = parseInt(prompt("這餐花了多少錢??"))
+// let tipPercentage = parseInt(prompt("小費的百分比?"))/100
+// let tipAmoumt = food * tipPercentage
+// console.log("小費金額" + tipAmoumt)
+// let total = food + tipAmoumt
+// console.log("總金額是" + total)
+
+var y = 20;
+    y -= 7; // y = y-7
+    console.log('y等於' , y);
+
+var x = 10;
+    x += 1; // x = x+1
+    console.log('x等於' , x);
+    x *= 5 // x = x * 5
+    console.log(x);
+    x /= 11 // x = x/11
+    console.log('新的x等於', x);
+
+    //由上而下照順序改變x的值
+
+    var z = 35;
+    z %= 10 // z = z % 10
+    console.log(z);
+
+    //次方 **
+    var a = 2;
+    console.log(a ** 5);
+    
+    
